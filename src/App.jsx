ca8e1ca20302a1b2ff26e0f0cab0973be8636bcb
trimbak <akshaypaithane45@gmail.com>
2025-03-17 12:37:34 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+		<br><marquee>Created by Akshay Paithane...</marquee>
       </p>
     </>
   )
